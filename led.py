2dd50393c6af485a73b3c759d458964d8265ecea
Nino Filiu <nino.filiu@gmail.com>
2023-10-11 15:08:13 +0100

    color ramp

--- led.py
+++ led.py
@@ -1,19 +1,30 @@
 import board
 import neopixel
 import time
+import math
 
 nb_leds = 720
-
 pixels = neopixel.NeoPixel(board.D18, nb_leds, auto_write=False)
 
 cold = [0,0,50]
 mid = [50,15,0]
 hot = [255,5,0]
 
-# interpolate colors
-def interpolate(a, b):
-	return [a + t * (b-a) for (a,b) in zip(cold, mid)]
+def mix(t, a, b):
+	print(t, a, b)
+	return [a + t * (b-a) for (a,b) in zip(a,b)]
 
+def color_ramp(t, ramp):
+	if (t==1):
+		return ramp[-1]
+	n = len(ramp) - 1
+	i = math.floor(t * n)
+	u = (t * n) % 1
+	return mix(u, ramp[i], ramp[i+1])
+
+print(color_ramp(0.75, [cold, mid, hot]))
+
+"""
 for i in range(nb_leds):
 	t = None
 	a = None
@@ -28,3 +39,4 @@ for i in range(nb_leds):
 		b = mid
 	pixels[i] = interpolate(a, b)
 pixels.show()
+"""
